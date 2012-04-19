5dc6c2ba76339599335ce42f7b9d670e21ad0760
weizhuo <wuweizhuo@gmail.com>
2012-04-19 18:46:07 -0300

    Update RSALibrary.c

--- RSALibrary.c
+++ RSALibrary.c
@@ -3,11 +3,22 @@
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
+
+
+struct Arguments {
+    int connfd;
+    long my_e;
+    long my_c;
+    long my_d;
+    long you_e;
+    long you_c;
+    int run;
+};
 /*
  * returns 0 if an error occurs, returns 1 if everything is fine
  */
 
-int extractTwos(int m ,int *s, int *d) {
+long extractTwos( long m , long *s,  long *d) {
     if (s == NULL  && d == NULL) {
         return 0;
     }
@@ -16,16 +27,16 @@ int extractTwos(int m ,int *s, int *d) {
     while(m % 2 == 0) {
         m = m / 2;
         *s += 1;
-      
+
     }
     *d = m;
     return 1;
 
 }
 
-int int2BaseTwo (int x, int *c) {
-    int numOfDigits = 1 + log2(x);
-    int i = 0;
+ long  long2BaseTwo ( long x,  long *c) {
+     long numOfDigits = 1 + log2(x);
+     long i = 0;
 
     if (c == NULL) {
         return 0;
@@ -33,148 +44,114 @@ int int2BaseTwo (int x, int *c) {
     for(i = 0; i < numOfDigits ; i++) {
         c[i] = x % 2;
         x = x / 2;
-       // printf("x = %d", x);
-  
+     
     }
-//      c[numOfDigits] = '\0';
-
     return 1;
 
 }
 
-int GCD(int a, int b) {
-    int remainder = 1;
+ long GCD( long a,  long b) {
+     long remainder = 1;
+
+    if(a == 0)
+       return b;
+    if (b == 0)
+       return a; 
     while(remainder != 0) {
         remainder = a % b;
         a = b;
         b = remainder; 
+
     }
     return a;
 }
 
-int coprime(int x) {
-    int random, i;
-    for(i = 0; i < 200; i++)  {
-        random = rand() % x + 2;
-        //printf("rannd = %d\n", random);
-        if(GCD(x, random) == 1) 
-            return random;
+ long coprime( long x) {
+     long random, i;
+    
+    for(i = 0; i < 200000; i++)  {
+        random = rand() % 1000;
+        if(random > 1) {
+
+            if(GCD(x, random) == 1) 
+                return random;
+
+        }
     }
+    return -1; 
 
 }
 
-long modulo(int a, int d, int n) {
-    int *c;
-    int *auxc;
-    int numOfDigits = 1 + log2(d);
-    int i = 0;
+/*
+long modulo(long a, long d, long n) {
     long retValue = 1;
-    c = (int *)malloc((numOfDigits )* sizeof(int)); 
-    auxc = (int *)malloc((numOfDigits )* sizeof(int)); 
-    int2BaseTwo(d, c);
-    for (i = 0; i < numOfDigits; i++) {
-       // printf("%d\n", c[i]);
-    }
-    auxc[0] = a % n;
-    for (i = 1; i < numOfDigits; i++) {
-    auxc[i] = ((int)pow(auxc[i - 1], 2)) % n;
+    printf("bruteforce\n");
+    int i  = 0; 
+    for(i = 0; i < d ; i++) {
+    printf("bruteforce\n");
+    retValue = a * retValue;
+    retValue = retValue % n;
+    }  
+    return a;
+}*/
 
+long modulo(long a,  long d,  long n) {
+    long *c;
+    long *auxc;
+    long numOfDigits = 1 + log2(d);
+    long i = 0;
+    long retValue = 1;
+    long remainder = 1;
+    c = ( long *)malloc((numOfDigits )* sizeof( long)); 
+    auxc = ( long *)malloc((numOfDigits )* sizeof( long)); 
+    long2BaseTwo(d, c);
+
+    auxc[0] = a % n;
+    remainder = auxc[0]; 
+    for (i = 1; i < numOfDigits; i++) {
+        remainder = (remainder * remainder) % n;
+        auxc[i] = remainder;
+        if(auxc[i] < 0)  auxc[i] += n; 
     }
     for (i = 0; i < numOfDigits; i++) {
-        if (c[i]) {
-            retValue = retValue * auxc[i] % n;
+        if (c[i] == 1) {
+            retValue = (retValue * auxc[i]) % n;
         }
     }
     free(c);
     free(auxc);
-
     return retValue;
 }
-/*
- * returns 0 if its not prime else returns 1
- */
-int millerRabin(int n, int k) {
-    if ((n % 2) == 0)
-         return 0;
 
-    int s = 0;
-    int d = 0;
-    int i = 0;
-    int j = 0;
-    int randNum = 0;
-    int temp = 0;
-    srand(time(NULL));
-   
-    extractTwos(n , &s, &d);
-    printf("s = %d", s); 
-    printf("d = %d", d);
-    for (i = 0; i < k; i++) {
-        randNum = (rand() % (n - 2)) + 2;
-       // printf("%d\n", randNum); 
-  temp = modulo(randNum, d, n);
-        if(temp == 1 || temp == n - 1)
-            continue;
-        for(j = 0; j < s; j++) {
-            temp = modulo(temp, 2, n);
-            if (temp == 1)
-                 return 0;
-            if (temp == n - 1)
-                 break;
-            if (j == s - 1)
-                return 0;
-            j += 1;
 
-        }
+
+long extendedEuclid( long a,  long b,  long *u1,  long *u2,  long *u3) {
+    int v1, v2, v3, t1, t2, t3;
+    int quotient;
+    *u1 = 1;
+    *u2 = 0;
+    *u3 = a;
+    v1 = 0;
+    v2 = 1;
+    v3 = b;
+    while(v3 != 0) {
+        quotient = *u3 / v3;
+        t1 = *u1 - quotient * v1;
+        t2 = *u2 - quotient * v2;
+        t3 = *u3 - quotient * v3;
+        *u1 = v1;
+        *u2 = v2;
+        *u3 = v3;
+        v1 = t1;
+        v2 = t2;
+        v3 = t3; 
  
-
     }
-    return 1;
-}
-
-/*
- * returns -1 if no inverse exists else returns the modular inverse
- */
-
-
-int extendedEuclid(int a, int b, int *d, int *x, int *y) {
-    
-    int  x1, x2,  y2, y1; 
-    if(b == 0) {
-       *d = a;
-       *x = 1;
-       *y = 0;
-       return 1;   
-
-    }
-    x2 = 1;
-    x1 = 0;
-    y2 = 0;
-    y1 = 1;
-    int quotient; int remainder;
-    while (b > 0) {
-         quotient = a / b;
-         remainder = a - (quotient) * b;
-         *x = x2 - quotient * x1;
-         *y = y2 - quotient * y1;
-         a = b;
-         b = remainder;
-         x2 = x1;
-         x1 = *x;
-         y2 = y1;
-         y1 = *y; 
-
-    }
-    *d = a;
-    *x = x2;
-    *y = y2;
-    
-    return 1;
-    
  
 }
 
-int isPrime(int x) {
-    int i = 0;
+long isPrime( long x) {
+    long i = 0;
     for(i = 2; i  < x; i++) {
         if((x % i) == 0)
             return 0; 
@@ -183,25 +160,20 @@ int isPrime(int x) {
 }
 
 
-int totient(int n) {
-    //first find all possible prime numbers between printf("My private key: %d, %d\n", args->my_d, args->my_c);2 and n
-    int *primeList  = (int *)malloc(n * sizeof(int));
+long totient( long n) {
+    long *primeList  = ( long *)malloc(n * sizeof( long));
     primeList[0] = 2;
-    int j = 0;
-    int p;
-    int q;
-    int index = 1;
-    int i = 2;
+    long j = 0;
+    long p;
+    long q;
+    long index = 1;
+    long i = 2;
     for(i = 3; i < n; i +=2) {
         if(isPrime(i) == 1) {
             primeList[index] = i;
             index++;
         }
     } 
-    for(i = 0; i < index; i++) {
-       // printf("prime = %d\n", primeList[i]);
-
-    }
     //now we have the list in primeList, brute force to find the primeNUmbers
     for(i = 0; i < index; i++) {
         for (j = 0 ; j  <index; j++) {
@@ -217,97 +189,64 @@ int totient(int n) {
     return -1;
 }
 
-
-int encrypt(int input, int e, int c) {
-return modulo(input, e, c);
-
+long encrypt( long input,  long e,  long c) {
+    long retValue;
+    retValue = modulo(input, e,c);
+    return retValue;
 }
-
-
-/*
- * returns -1 if numbers are not co-prime
- * else returns the modular inverse
- */
-
-
-int mod_inverse(int base, int m) {
-    int d, x, y;
-    int retValue = 0;
+ 
+long mod_inverse( long base,  long m) {
+    long d, x, y;
+    long retValue = 0;
     d = 1;
     x = 1;
     y = 1;
     
-    if(GCD(base, m) == 1) {
-        extendedEuclid(base, m, &d, &x, &y);
-        retValue =  x % m;
-        if(retValue < 0) 
-           retValue = retValue + m;
+    if(GCD(base, m) == 1) {  
+        extendedEuclid( base, m,  &x, &y, &d);
+        retValue =  x;
+        if(retValue < 0) {
+           retValue = retValue +  m;
+        }
         return retValue; 
     }
     return -1;
 }
 
-char decrypt(int input, int d, int c) {
-    printf("%d decrypted using d=%d,c=%d as %d", input, d, c, modulo(input, d, c));
-    return modulo(input, d, c);
+long decrypt( long input,  long d,  long c) {
+    long retValue = modulo(input, d, c);
+    return retValue;
 }
 
-int generatePrimeNumbers(int *m, int *n) {
-    int maximum = *m > *n ? *m : *n;
-    int *primeList = (int *)malloc(maximum * sizeof(int));
-    int index = 1; 
-    int i = 3;
+long generatePrimeNumbers( long *m,  long *n) {
+    long maximum = *m > *n ? *m : *n; 
+    long *primeList = ( long *)malloc((maximum) * sizeof( long));
+    long index = 1; 
+    long i = 3;
+     
     primeList[0] = 2;
     while(index < maximum) {
         if (isPrime(i) == 1) {
             primeList[index] = i;
-            index++;
-            
+            index++;         
         }
         i += 2; 
     }
-    
     *m = primeList[*m - 1];
     *n = primeList[*n - 1];     
     free(primeList);
 }
-/*
-void main() {
-srand(time(NULL));
-char *input = "Hello";
-int *encOutput = (int *)malloc(sizeof(int) * strlen(input) + 1);
-int c = 13 * 17;
-int i; 
-int a = 13;
-int b = 17;
-int e = coprime(12 * 16);
-int m = totient(13 * 17);
-int d = mod_inverse(e, 12 * 16); 
-printf("totient = %d", totient(13 * 17));
-printf("e = %d\n", coprime(12 * 16));
 
 
-
-printf("d = %d\n", mod_inverse(coprime(12 * 16), 12 * 16));
-int enc = encrypt(50, e,c);
-int dec = decrypt(enc, d, c);
-printf("enc = %d\n", enc);
-printf("dec = %d\n", dec);
-
-
-for(i = 0; i < strlen(input); i++) {
-    encOutput[i] = encrypt(input[i], e, c);
-     
-  
+void generateKeys(long a, long b, long *e, long *d, long *c) {
+    *c = a * b;
+    *e = coprime( (a - 1) * (b - 1));
+    *d = mod_inverse(*e,  ((a - 1) * (b - 1)));
 }
 
-printf("Hello has been translated into %d %d %d %d %d\n", encOutput[0], encOutput[1], encOutput[2], encOutput[3], encOutput[4]);
 
+void printKeys(struct Arguments args) {
+  
+    printf("PRINTING KEYS: my_e = %ld, my_d = %ld, my_c = %ld you_e %ld   you_c = %ld\n", args.my_e, args.my_d, args.my_c, args.you_e, args.you_c);
 
-//    printf("coprime=%d\n",mod_inverse(3, 30));
-//printf("%d", modulo(2349723,423424,12345));
-//printf("mod inv = %d\n", extendedEuclid(442, 2278, &d, &x, &y));
-//printf("d = %d \t x = %d \t y = %d\n", d,x,y);
-//coprime(5);
-
-}*/
+}
