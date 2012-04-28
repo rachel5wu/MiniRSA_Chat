197e323cf7f58cef815431cbe4ec86c0fc3ae5f5
weizhuo <wuweizhuo@gmail.com>
2012-04-28 00:03:20 -0300

    Update client.c

--- client.c
+++ client.c
@@ -16,7 +16,8 @@ int main(int argc, char** argv) {
         generatePrimeNumbers(&a, &b);
         generateKeys(a, b, &args.my_e, &args.my_d, &args.my_c);
         args.run = 1;  
-	
+	args.who = 1;
+
         fd = connectTo(argv[2], atoi(argv[1]));
         args.connfd = fd;
         sendKeys(&args);
@@ -25,7 +26,8 @@ int main(int argc, char** argv) {
         pthread_create(&readThread, NULL, readData, &args);
         pthread_create(&writeThread, NULL, writeData, &args);
         pthread_join(readThread, NULL);
-        pthread_cancel(writeThread);
+       // pthread_cancel(writeThread);
 	close(fd);
+        exit(0);
 }
 
