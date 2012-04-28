5dc83c3ab9925f8ef82714a05ac901b7462fa6be
weizhuo <wuweizhuo@gmail.com>
2012-04-28 00:04:29 -0300

    Update server.c

--- server.c
+++ server.c
@@ -1,36 +1,36 @@
 #include "readWriteData.h"
 
 int main(int argc, char** argv) {
-        srand(time(NULL)); 
-        pthread_t readThread, writeThread;
-        struct Arguments args;
+    srand(time(NULL)); 
+    pthread_t readThread, writeThread;
+    struct Arguments args;
 	int srvfd, connfd;
 	char buffer[256];
 	long a, b; // two prime numbers
 	char base[256];
-        int i;
-        long enc, dec;
+    int i;
+    long enc, dec;
 	if(argc < 4) {
 	   printf("file portNum prime1 prime2\n");
 	   exit(0);
 	}
-        a = atoi(argv[2]) + 6;
-        b = atoi(argv[3]) + 6; 
-        generatePrimeNumbers(&a, &b);
-        generateKeys(a, b, &args.my_e, &args.my_d, &args.my_c);
-        args.run = 1;
-
+    a = atoi(argv[2]) + 6;
+    b = atoi(argv[3]) + 6; 
+    generatePrimeNumbers(&a, &b);
+    generateKeys(a, b, &args.my_e, &args.my_d, &args.my_c);
+    args.run = 1;
+        args.who = 0;
 	srvfd = makeListener(atoi(argv[1]));
-	connfd = listenFor(srvfd);
+    while(1) {
+	    connfd = listenFor(srvfd);
         args.connfd = connfd;         
         sendKeys(&args);
         receiveKeys(&args);
-
+        args.run = 1;
         pthread_create(&readThread, NULL, readData, &args);
         pthread_create(&writeThread, NULL, writeData, &args);
-        pthread_join(readThread, NULL);
-        pthread_cancel(writeThread);
-        close(connfd);
-        close(srvfd);
+         
+    }
+    close(srvfd);
 }
  
